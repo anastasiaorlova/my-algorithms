3da684d74449b360d619f578ccaefe611c3e28f8
residenttrickster <metkusblackus@gmail.com>
2020-09-08 17:21:32 -0400

    Recursion

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //my algorithm studies 
+
+function power(base, exponent){
+    if(exponent === 0) return 1;
+    return base * power(base,exponent-1);
+}
+
+
+function factorial(x){
+    if (x < 0 ) return 0;
+    if (x <= 1 ) return 1;
+    return x * factorial(x-1);
+}
+
+function productOfArray(arr) {
+    if(arr.length === 0) {
+        return 1;
+    }
+    return arr[0] * productOfArray(arr.slice(1));
+}
