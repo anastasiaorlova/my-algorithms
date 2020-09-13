631eb86d079eb6fb87b73aad37c20cac08bdbaeb
residenttrickster <metkusblackus@gmail.com>
2020-09-13 19:02:38 -0400

    Selection Sort

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function productOfArray(arr) {
     return arr[0] * productOfArray(arr.slice(1));
 }
 
+//linear search
+
 function linearSearch(arr, val) {
 
     for(let i = 0; i< arr.length; i++) {
@@ -30,6 +32,7 @@ function linearSearch(arr, val) {
     }
 }
 
+//binary Search
 function binarySearch(arr, elem) {
     let start = 0;
     let end = arr.length - 1;
@@ -48,9 +51,11 @@ function binarySearch(arr, elem) {
     return -1;
 }
 
+//Bubble sort
+
 function bubbleSort(arr) {
-    for(i = arr.length; i > 0; i--) {
-        for(j = 0; j < i-1; j++) {
+    for(let i = arr.length; i > 0; i--) {
+        for(let j = 0; j < i-1; j++) {
             if(arr[j] > arr[j+1]) {
                 let swap = arr[j];
                 arr[j] = arr[j+1];
@@ -62,3 +67,42 @@ function bubbleSort(arr) {
 }
 
 
+//Selection sort 
+// LEGACY VERSION (non ES2015 syntax)
+function sselectionSort(arr){
+    for(var i = 0; i < arr.length; i++){
+        var lowest = i;
+        for(var j = i+1; j < arr.length; j++){
+            if(arr[j] < arr[lowest]){
+                lowest = j;
+            }
+        }
+        if(i !== lowest){
+            //SWAP!
+            var temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+    }
+    return arr;
+}
+
+// ES2015 VERSION
+function selectionSort(arr) {
+const swap = (arr, idx1, idx2) =>
+([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+for (let i = 0; i < arr.length; i++) {
+let lowest = i;
+for (let j = i + 1; j < arr.length; j++) {
+    if (arr[lowest] > arr[j]) {
+    lowest = j;
+    }
+}
+if (i !== lowest) swap(arr, i, lowest);
+}
+
+return arr;
+}
+
+selectionSort([0,2,34,22,10,19,17]);
