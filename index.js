2f63065561db48bf536d8fbe0e84c1b8bc48415a
residenttrickster <metkusblackus@gmail.com>
2020-10-01 11:04:55 -0400

    Review

--- index.js
+++ index.js
@@ -296,15 +296,5 @@ class Node {
     }
 }
 
-class BinarySearchTree {
-    constructor(){
-        this.root = null;
-    }
-}
 
-var tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
 
