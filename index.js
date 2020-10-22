7bd9c7393aa424009da7856eb00aaa4a734d4ead
residenttrickster <metkusblackus@gmail.com>
2020-10-21 21:05:19 -0400

    Review Lern.co

--- index.js
+++ index.js
@@ -400,3 +400,14 @@ if (s.length >= min
 
 return valleys; 
 }
+
+//Merge Sort
+function merge(firstHalf, secondHalf){
+let sorted = []
+let currentMin;
+while(firstHalf.length != 0 && secondHalf.length != 0){
+    let currentMin = findMinAndRemove(firstHalf, secondHalf)
+    sorted.push(currentMin)
+}
+return sorted.concat(firstHalf).concat(secondHalf)
+}
