be6b8acc97bfe37ad776885a889369bbfc1234b7
residenttrickster <metkusblackus@gmail.com>
2020-10-24 19:28:37 -0400

    Review Lern.co

--- index.js
+++ index.js
@@ -59,36 +59,6 @@ function sockMerchant(n, ar) {
     return pairs;
 }
 
-function countingValleys(n, s) {
-const min = 2;
-const max = 1000000;
-let isInValley = false;
-let valleys = 0;
-s = (typeof s === "string") ? s.split('') : s;
-
-if (s.length >= min
-        && s.length <= max
-        && n === parseInt(n, 0)
-        && n >= min
-        && n <= max 
-        && n === s.length) {
-        
-        s.map(steps => ((steps === "U") ? 1 : -1))
-            .reduce((prev, next) => {
-                if (prev < 0 && !isInValley) {
-                    isInValley = true;
-                }
-                if ((prev + next) === 0 && isInValley) {
-                    valleys++;
-                    isInValley = false;
-                }
-
-                return prev + next;    
-            });
-} 
-
-return valleys; 
-}
 
 //Merge Sort
 function merge(firstHalf, secondHalf){
