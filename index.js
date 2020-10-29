c8b515574acb934e1e3120edb948c6ebee114f31
residenttrickster <metkusblackus@gmail.com>
2020-10-28 21:49:53 -0400

    Lear.co

--- index.js
+++ index.js
@@ -60,3 +60,29 @@ if(array.length < 2){
 }
 }
 
+//for two arrays
+
+let firstHalf =  [1, 2, 6, 7, 8]
+let secondHalf =  [3, 4, 5, 9, 10]
+
+function findMinAndRemove(firstHalf, secondHalf) {
+    let minfirstHalf = firstHalf[0];
+    let minsecondHalf = secondHalf[0];
+
+    if(minfirstHalf < minsecondHalf) {
+        return firstHalf.shift()
+    } else {
+        return secondHalf.shift()
+    }
+}
+
+function merge(firstHalf, secondHalf) {
+    let sortedArr = [];
+    let currentMin;
+    while(firstHalf.length != 0 && secondHalf.length != 0) {
+        let currentMin = findMinAndRemove(firstHalf. secondHalf)
+        sorted.push(currentMin)
+    }
+    return sorted.concat(firstHalf).concat(secondHalf)
+}
+
