926ba501aa88ed509cbceeca88d2106567b86662
residenttrickster <metkusblackus@gmail.com>
2020-10-30 11:54:29 -0400

    Hakerrank Day1

--- index.js
+++ index.js
@@ -93,3 +93,30 @@ while (d) {
     d--;
     }
 }
+
+//Hakerrank Day 1
+
+function main() {
+    let i = 4
+    let d = 4.0
+    let s = "HackerRank "
+    // Declare second integer, double, and String variables.
+    let i1;
+    let d1;
+    let s1;
+    // Read and save an integer, double, and String to your variables.
+    i1 = parseInt(readLine());
+    d1 = parseFloat(reaLine());
+    s1 = readLine();
+
+    // Print the sum of both integer variables on a new line.
+    console.log(i + i1);
+
+    // Print the sum of the double variables on a new line.
+    console.log(d + d1);
+
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s + s1)
+
+}
