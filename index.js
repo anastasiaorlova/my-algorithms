4081ce36aec1b48302d1aaa69331ae220ef04038
residenttrickster <metkusblackus@gmail.com>
2020-10-31 19:54:34 -0400

    template

--- index.js
+++ index.js
@@ -120,3 +120,35 @@ function main() {
     console.log(s + s1)
 
 }
+
+//Anagram template
+
+function isAnagram(stringA, stringB) {
+
+    function createCharMap(text) {
+        let charMap = {}
+
+        for (let char of text) {
+            if (charMap.hasOwnProperty(char)) {
+                charMap[char]++
+            } else {
+                charMap[char] = 1
+            }
+        }
+
+        return charMap
+    }
+
+    if (stringA.length === stringB.length) {
+        let stringAMap = createCharMap(stringA)
+        let stringBMap = createCharMap(stringB)
+        for (let char in stringAMap) {
+            if (stringAMap[char] !== stringBMap[char]) {
+                return false
+            }
+        }
+        return true
+    } else {
+        return false
+    }
+}
