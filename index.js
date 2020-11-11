c12de0c499ea53167e31a1d50602aa59463e3339
residenttrickster <metkusblackus@gmail.com>
2020-11-11 12:59:31 -0500

    Day 2 Hakerrank

--- index.js
+++ index.js
@@ -191,3 +191,11 @@ function maximumToys(prices, k) {
     }
     return bought
 }
+
+//Day 2 Hakerrank
+function solve(meal_cost, tip_percent, tax_percent) {
+
+    let total_cost;
+    total_cost =  meal_cost + meal_cost * tip_percent/100 + meal_cost * tax_percent/100;
+    console.log(Math.round(total_cost));
+}
