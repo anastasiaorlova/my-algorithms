79614671ed1245ef7bc057414d7ca4e4efbd2c18
residenttrickster <metkusblackus@gmail.com>
2021-01-01 19:36:01 -0500

    Add list

--- index.js
+++ index.js
@@ -127,28 +127,6 @@ function isAnagram(stringA, stringB) {
     }
 }
 
-//Linked List
-class Node {
-    constructor(data) {
-        this.data = data
-        this.next = null                
-    }
-}
-
-class LinkedList {
-    constructor(head = null) {
-        this.head = head
-    }
-}
-
-let node1 = new Node(2)
-let node2 = new Node(5)
-node1.next = node2
-
-let list = new LinkedList(node1)
-
-console.log(list.head.next.data) //returns 5
-
 
 //Mark and toys: Hakerrank
 function maximumToys(prices, k) {
